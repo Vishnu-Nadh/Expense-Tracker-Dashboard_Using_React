8c453aa165d1f032d9b915975f76aae5488bfda0
Vishnu-Nadh <vishnunadh222@gmail.com>
2022-05-09 16:38:26 +0530

    initial setup finished

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h3 className="bg-red-500 text-2xl font-bold">
+        This is just a heading to checkout
+      </h3>
+    </div>
+  );
+}
+
+export default App;
